1769c2a1d870250c8dee91257de752fa547620b0
Martin <martingakuya@gmail.com>
2023-07-29 14:37:38 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    const form = document.getElementById("create-task-form");
+    const tasksList = document.getElementById("tasks");
+    const prioritySelect = document.getElementById("priority");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const taskInput = document.getElementById("new-task-description");
+      const taskText = taskInput.value;
+  
+      // Get the selected priority from the dropdown
+      const priorityValue = prioritySelect.value;
+  
+      const taskElement = document.createElement("li");
+      taskElement.textContent = taskText;
+  
+      // Add a class based on the selected priority to change text color
+      taskElement.classList.add(priorityValue);
+  
+      // Add a delete button to the task element
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function() {
+        tasksList.removeChild(taskElement);
+      });
+      taskElement.appendChild(deleteButton);
+  
+      tasksList.appendChild(taskElement);
+  
+      taskInput.value = "";
+    });
+  });
+  
